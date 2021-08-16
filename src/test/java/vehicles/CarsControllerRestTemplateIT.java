d29017f05ad41e374a86b5de163f0663db80f25e
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-08-16 21:23:06 +0200

    Fifth commit

--- src/test/java/vehicles/CarsControllerRestTemplateIT.java
+++ src/test/java/vehicles/CarsControllerRestTemplateIT.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,9 +31,9 @@ public class CarsControllerRestTemplateIT {
 
         carsService.deleteAll();
 
-        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 279000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
         CarDto carDto2 = template.postForObject("/api/cars/", new CreateCarCommand("BMW", "I3", 2016, 84682, ConditionOfTheVehicle.EXCELLENT, EngineType.ELECTRIC, NumberOfSeats.FOUR_SEATER, 6100000, "Jane", "Doe", "+3630/323-4567", "BMW1463787890DEFH"), CarDto.class);
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
 
@@ -60,7 +61,7 @@ public class CarsControllerRestTemplateIT {
     void testSelectById() {
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         CarDto foundCar = template.exchange("/api/cars/2", HttpMethod.GET, null, CarDto.class).getBody();
@@ -71,9 +72,9 @@ public class CarsControllerRestTemplateIT {
     void testFindOwnerById() {
         carsService.deleteAll();
 
-        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 279000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
         CarDto carDto2 = template.postForObject("/api/cars/", new CreateCarCommand("BMW", "I3", 2016, 84682, ConditionOfTheVehicle.EXCELLENT, EngineType.ELECTRIC, NumberOfSeats.FOUR_SEATER, 6100000, "Jane", "Doe", "+3630/323-4567", "BMW1463787890DEFH"), CarDto.class);
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
 
@@ -85,7 +86,7 @@ public class CarsControllerRestTemplateIT {
     void testSelectByMake() {
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         List<CarDto> foundCarByMake = template.exchange("/api/cars/make?make=CheVrolET", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
@@ -98,7 +99,7 @@ public class CarsControllerRestTemplateIT {
     void testSelectByModel() {
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         List<CarDto> foundCarByModel = template.exchange("/api/cars/model?model=MX-5", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
@@ -111,7 +112,7 @@ public class CarsControllerRestTemplateIT {
     void testSelectByCondition() {
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         List<CarDto> foundCarByModel = template.exchange("/api/cars/condition?condition=USED", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
@@ -125,8 +126,8 @@ public class CarsControllerRestTemplateIT {
 
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
-        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 279000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         List<CarDto> foundCarByEngine = template.exchange("/api/cars/engine?engine=DIESEL", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
@@ -141,8 +142,8 @@ public class CarsControllerRestTemplateIT {
 
         carsService.deleteAll();
 
-        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 214571, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
-        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 279000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
         CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
 
         List<CarDto> foundCarByNumberOfSeats = template.exchange("/api/cars/numberOfSeats?numberOfSeats=SEVEN_SEATER", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
@@ -151,6 +152,75 @@ public class CarsControllerRestTemplateIT {
         assertThat(foundSeats).isEqualTo("SEVEN_SEATER");
         assertThat(foundCarByNumberOfSeats.size()).isEqualTo(1);
         assertThat(foundCarByNumberOfSeats.get(0).getModel()).isEqualTo("Captiva");
+    }
+
+    @Test
+    void testSelectByYear() {
+
+        carsService.deleteAll();
+
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
+
+        List<CarDto> foundCarsByYear = template.exchange("/api/cars/year?year=2001-2006", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
+        }).getBody();
+        assertThat(foundCarsByYear).extracting(CarDto::getMake).contains("AUDI", "Mazda");
+        assertThat(foundCarsByYear.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    void testSelectByPrice() {
+
+        carsService.deleteAll();
+
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
+
+        List<CarDto> foundCarsByPrice = template.exchange("/api/cars/price?price=2140000-2790000", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
+        }).getBody();
+        assertThat(foundCarsByPrice).extracting(CarDto::getMake).contains("AUDI", "Chevrolet");
+        assertThat(foundCarsByPrice.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    void testUpdatePrice() {
+        carsService.deleteAll();
+
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+
+        CarDto originalPricedCar = template.exchange("/api/cars/1", HttpMethod.GET, null, CarDto.class).getBody();
+
+        template.put("/api/cars/1", new UpdateCarPriceCommand(4280000));
+        CarDto newPricedCar = template.exchange("/api/cars/1", HttpMethod.GET, null, CarDto.class).getBody();
+        assertTrue((originalPricedCar.getPrice() * 2) == newPricedCar.getPrice());
+    }
+
+
+    @Test
+    void testForDelete() {
+
+        carsService.deleteAll();
+
+        CarDto carDto3 = template.postForObject("/api/cars/", new CreateCarCommand("Chevrolet", "Captiva", 2013, 214571, ConditionOfTheVehicle.EXCELLENT, EngineType.GASOLINE, NumberOfSeats.SEVEN_SEATER, 2140000, "Jack", "Smith", "+3630/423-4567", "Chevrolet7890DEFH"), CarDto.class);
+        CarDto carDto1 = template.postForObject("/api/cars/", new CreateCarCommand("AUDI", "S6", 2001, 239713, ConditionOfTheVehicle.USED, EngineType.DIESEL, NumberOfSeats.FIVE_SEATER, 2790000, "John", "Doe", "+3630/123-4567", "AUD1463787890DEFH"), CarDto.class);
+        CarDto carDto4 = template.postForObject("/api/cars/", new CreateCarCommand("Mazda", "MX-5", 2006, 119626, ConditionOfTheVehicle.USED, EngineType.GASOLINE, NumberOfSeats.TWO_SEATER, 2900000, "Bill", "Smith", "+3630/523-4567", "MAZDAABC5234567890DEFH"), CarDto.class);
+
+        List<CarDto> foundCarsExistsBeforeDelete = template.exchange("/api/cars/", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
+        }).getBody();
+        assertThat(foundCarsExistsBeforeDelete.size()).isEqualTo(3);
+
+        template.delete("/api/cars/2");
+
+        List<CarDto> foundCarsStillExists = template.exchange("/api/cars/", HttpMethod.GET, null, new ParameterizedTypeReference<List<CarDto>>() {
+        }).getBody();
+
+        assertThat(foundCarsStillExists).extracting(CarDto::getMake).contains("Mazda", "Chevrolet");
+        assertThat(foundCarsStillExists.size()).isEqualTo(2);
+
     }
 
 
